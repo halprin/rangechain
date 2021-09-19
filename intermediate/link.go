260c541d2bf69192bc9b5363bd46c0bd3681aa15
halprin <halprin@users.noreply.github.com>
2021-09-19 09:08:12 -0600

    Add filter

--- intermediate/link.go
+++ intermediate/link.go
@@ -9,3 +9,17 @@ func NewLink(generator func() int) *Link {
 		generator: generator,
 	}
 }
+
+func (receiver *Link) filter(filterFunction func(int) bool) *Link {
+	filterGenerator := func() int {
+		//go through the generator until you find an item that stays
+		for {
+			valueToFilter := receiver.generator()
+			if filterFunction(valueToFilter) {
+				return valueToFilter
+			}
+		}
+	}
+
+	return NewLink(filterGenerator)
+}
