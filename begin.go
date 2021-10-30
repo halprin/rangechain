8260f7d8d74b79f73bfd6f19faa7fdef7ae5fa8d
halprin <halprin@users.noreply.github.com>
2021-10-30 16:10:50 -0600

    Add code documentation to top level package

--- begin.go
+++ begin.go
@@ -5,7 +5,8 @@ import (
 	"github.com/halprin/rangechain/intermediate"
 )
 
-// FromSlice starts the chain with the supplied slice.  Chaining and terminating methods can now be called on the result.
+// FromSlice starts the chain with the supplied slice.
+// Chaining and terminating methods can now be called on the result.
 func FromSlice(slice interface{}) *intermediate.Link {
 	sliceGenerator := generator.FromSlice(slice)
 
@@ -13,6 +14,8 @@ func FromSlice(slice interface{}) *intermediate.Link {
 	return link
 }
 
+// FromArray starts the chain with the supplied array.
+// Chaining and terminating methods can now be called on the result.
 func FromArray(array interface{}) *intermediate.Link {
 	arrayGenerator := generator.FromArray(array)
 
@@ -20,6 +23,8 @@ func FromArray(array interface{}) *intermediate.Link {
 	return link
 }
 
+// FromChannel starts the chain with the supplied channel.
+// Chaining and terminating methods can now be called on the result.
 func FromChannel(channel interface{}) *intermediate.Link {
 	channelGenerator := generator.FromChannel(channel)
 
@@ -27,6 +32,8 @@ func FromChannel(channel interface{}) *intermediate.Link {
 	return link
 }
 
+// FromMap starts the chain with the supplied map.
+// Chaining and terminating methods can now be called on the result.  The singular value used to represent the key and value pairs is `generator.MapTuple` of `github.com/halprin/rangechain/generator`.
 func FromMap(aMap interface{}) *intermediate.Link {
 	mapGenerator := generator.FromMap(aMap)
 
