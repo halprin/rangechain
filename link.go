4b851967ee5da1bff070a9c5b77d13a03e687036
halprin <halprin@users.noreply.github.com>
2021-10-31 16:47:28 -0600

    Uncomment unneeded comment

--- link.go
+++ link.go
@@ -5,7 +5,6 @@ type Link struct {
 	generator func() (interface{}, error)
 }
 
-// newLink is not meant to be called directly by external users.  Use the `From*` functions in the parent package rangechain.
 func newLink(generator func() (interface{}, error)) *Link {
 	return &Link{
 		generator: generator,
