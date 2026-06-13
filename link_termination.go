7b98548c5bae6cc5d492e4dbf790c08a6dd1d000
halprin <halprin@users.noreply.github.com>
2026-06-12 22:07:21 -0600

    use new new syntax

--- link_termination.go
+++ link_termination.go
@@ -2,8 +2,9 @@ package rangechain
 
 import (
 	"errors"
-	"github.com/halprin/rangechain/internal/generator"
 	"iter"
+
+	"github.com/halprin/rangechain/internal/generator"
 )
 
 // Slice serializes the chain into a slice and returns it.  Also returns an error if any previous chain method generated an error.  If an error is returned, the slice is filled in until the error was encountered.
@@ -148,8 +149,7 @@ func (receiver *Link[T]) Last() (*T, error) {
 			return lastValue, lastError
 		}
 
-		v := currentValue
-		lastValue = &v
+		lastValue = new(currentValue)
 		lastError = err
 	}
 }
